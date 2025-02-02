a0490ec4ad0fe9e1a17d0e01976c5919ac720bc5
Sarath Krishnan P V <pvsarath2002@gmail.com>
2025-02-02 13:01:12 +0530

    Update settings.py

--- foodproject/settings.py
+++ foodproject/settings.py
@@ -149,5 +149,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pvsarath2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'sodr hbmc wxtf mgin'
+EMAIL_HOST_PASSWORD = 'your_email_password'
 
